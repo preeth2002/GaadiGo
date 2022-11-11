d51992c8d30dc12fd83ba13a3d1ed400e0f949d0
Amudhan <sm.amudhan@live.com>
2022-11-12 03:10:29 +0530

    :sparkles: Add password hashing and login

--- js/signup.js
+++ js/signup.js
@@ -49,6 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Write data to firebase
 import { loginDetails, firebaseSuccessMsg } from "./firebase.js";
+import { hashPassword } from "./password-functions.js";
 
 const createAccountForm = document.getElementById("createAccountForm");
 createAccountForm.addEventListener("submit", createAccountViaForm);
@@ -56,13 +57,30 @@ createAccountForm.addEventListener("submit", createAccountViaForm);
 async function createAccountViaForm(e) {
   e.preventDefault();
 
-  loginDetails.add({
-    email: document.getElementById("email").value,
-    password: document.getElementById("password").value, //TODO: Hash passwords and store
+  // Check if email already exists
+  const email = document.getElementById("email").value;
+  var query = loginDetails.where("email", "==", email);
+  let result = await query.get();
+  if (!result.empty) {
+    alert("Email already exists.");
+    return;
+  }
+
+  // Hash Password for security
+  const hash_result = await hashPassword(
+    document.getElementById("password").value
+  );
+
+  const data = {
+    email: document.getElementById("email").value.toLowerCase(),
+    password: hash_result.hash,
+    salt: hash_result.salt,
     mobile: document.getElementById("mobile").value,
     age: document.getElementById("age").value,
     name: document.getElementById("full_name").value,
-  });
+  };
+
+  loginDetails.add(data);
 
   firebaseSuccessMsg();
   createAccountForm.reset();
